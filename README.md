8c8d817173696d25baef0d4484e3ce4e00cea546
Konrad Kine <konrad.kine@tutanota.com>
2023-04-16 21:51:05 +0200

    feat: add basic parameters and start/stop pos

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Spider Bot
 
 Simple robot with spider-like legs.
+
+## Parameters
+
+### Servos' minimals and maximals (MIN, MAX)
+
+Femur:
+1. FR → 150, 500 (good)                     [V]
+2. FL → 200, 550 (good)                     [V]
+3. BR → 60, 500 (good)                      [V]
+4. BL → 200, 550 (wrong turning direction)
+
+Tibia:
+1. FR → 200, 500 (good)                     [V]
+2. FL → 400, 550 (good)                     [V]
+3. BR → 60, 300 (bad low state)             [V]
+4. BL → 420, 590 (good)                     [V]
---
+++ main/main.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Array.h>
+
+// sets speed of servos' movement (more is slower)
+#define SERVO_DELAY 4
+
+#define SERVO_FREQ 60 // Analog servos run at ~50 Hz updates
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// there are 8 servos, 0-3: femur | 4-7: tibia
+
+// this are minimal values for femurs and tibias to be working
+int STARTPOS[8] = {
+  200,
+  550,
+  180,
+  420,
+  230,
+  400,
+  60,
+  420
+};
+
+// this are maximal values
+int STOPPOS[8] {
+  510,
+  250,
+  420,
+  200,
+  400,
+  550,
+  180,
+  590
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Servo test");
+
+  pwm.begin();
+
+  // don't know what this does B^)
+  pwm.setOscillatorFrequency(27000000);
+
+  pwm.setPWMFreq(SERVO_FREQ);
+}
+
+void loop() {
+
+}
